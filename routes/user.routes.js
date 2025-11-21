10c489d7b23715562dc743f0b4835dfa64f348bf
Rayan Haddad <rayan.py@proton.me>
2025-11-21 16:10:06 +0100

    Add a auth controller that includes sign(In/Up) functionnality and a user controller that injects a functionnality that gets users and user (by id) and calls them in the user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
-  res.send({
-    title: "GET all users",
-  });
-});
-userRouter.get("/:id", (req, res) => {
-  res.send({
-    title: "GET user's details",
-  });
-});
+userRouter.get("/", getUsers);
+userRouter.get("/:id", getUser);
 userRouter.post("/", (req, res) => {
   res.send({
     title: "CREATE all users",
